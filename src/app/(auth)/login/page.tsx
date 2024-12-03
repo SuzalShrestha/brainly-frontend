313ed6fc272eeb0c4a7a0dddbfcda12a5308a778
sujalshrestha <sujalshresthawork@gmail.com>
2024-12-03 19:46:23 +0545

    add suspense for build

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 import LoginCard from '@/components/login-card';
+import { Suspense } from 'react';
 
 function LoginPage() {
     return (
-        <div className='flex items-center justify-center h-full w-full'>
-            <LoginCard />
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className='flex items-center justify-center h-full w-full'>
+                <LoginCard />
+            </div>
+        </Suspense>
     );
 }
 export default LoginPage;
